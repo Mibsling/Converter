1fa048c394348daafa8a826d035e4af422fa2e1d
Mibsling <48335194+Mibsling@users.noreply.github.com>
2019-04-29 14:31:37 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(txtConverter.Text);
+            double Answer = Fah(amount);
+            lblConvert.Text = Answer.ToString() + " Fahrenheit";
+        }
+
+        private double Fah(double amount)
+        {
+            
+            double Answer = (amount * 9 / 5) + 32;
+            return Answer;  
+        }
+    }
+}
